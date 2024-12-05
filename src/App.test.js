9faa4df12c91f982540a444882bbffff2716d816
tinaerfanian <tinaerfanian95@gmail.com>
2024-12-05 15:44:52 +0100

    app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { render, screen } from '@testing-library/react';
+import Welcome from './components/Welcome';
+
+jest.mock('react-bootstrap', () => ({
+  Alert: ({ children }) => <div>{children}</div>,
+}));
+
+describe('Welcome Component', () => {
+  test('renders Welcome component with the correct heading text', () => {
+    render(<Welcome />);
+    const headingElement = screen.getByRole('heading', { level: 1 });
+    expect(headingElement).toBeInTheDocument();
+    expect(headingElement).toHaveTextContent('Benvenuti in EpiBooks!');
+  });
 });
